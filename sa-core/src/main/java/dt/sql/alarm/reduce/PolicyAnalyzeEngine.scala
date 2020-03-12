6ffe326f4c446bec5d06a3a0a9c13ef2c4e829c3
bebee4java <grsong.cn@gmail.com>
2020-03-12 11:54:36 +0800

    [fix bug] 🐛 keep redis cache ascending

--- sa-core/src/main/java/dt/sql/alarm/reduce/PolicyAnalyzeEngine.scala
+++ sa-core/src/main/java/dt/sql/alarm/reduce/PolicyAnalyzeEngine.scala
@@ -29,9 +29,11 @@ abstract class PolicyAnalyzeEngine {
           jobInfo =>
             val cache = cacheDf.filter(col(item_id) === jobInfo._1 and col(job_id) === jobInfo._2 and col(job_stat) === jobInfo._3)
               .select(col(SQL_FIELD_VALUE_NAME)).orderBy(col(event_time))
+              .repartition(1) // 重新partition 为了保证但分区写有序
+
             val key = AlarmPolicyConf.getCacheKey(jobInfo._1, jobInfo._2, jobInfo._3)
-            WowLog.logInfo(s"add cache records, key: $key, mode: ${mode.name}")
             RedisOperations.setListCache(key, cache, mode)
+            WowLog.logInfo(s"add cache records, key: $key, mode: ${mode.name}")
         }
       }
     } finally {
